93caff05c695fb9ad907c9538901d3a8f90167ce
AhnSangWoon <asw116@naver.com>
2024-05-28 21:56:59 +0900

    save video and meta data

--- fronted/youtube-ui/src/app/save-video-details/save-video-details.component.ts
+++ fronted/youtube-ui/src/app/save-video-details/save-video-details.component.ts
@@ -7,6 +7,7 @@ import {COMMA, ENTER} from "@angular/cdk/keycodes";
 import {ActivatedRoute} from "@angular/router";
 import {VideoService} from "../video.service";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {VideoDto} from "../video-dto";
 @Component({
   selector: 'app-save-video-details',
   templateUrl: './save-video-details.component.html',
@@ -27,6 +28,7 @@ export class SaveVideoDetailsComponent implements OnInit{
   videoId = '';
   fileSelected = false;
   videoUrl!: string
+  thumbnailUrl!: string
   announcer = inject(LiveAnnouncer);
 
   constructor(private activatedRoute: ActivatedRoute, private videoService: VideoService,
@@ -35,6 +37,7 @@ export class SaveVideoDetailsComponent implements OnInit{
     this.videoId = this.activatedRoute.snapshot.params.videoId;
     this.videoService.getVideo(this.videoId).subscribe(data => {
       this.videoUrl = data.videoUrl
+      this.thumbnailUrl = data.thumbnailUrl;
     })
     this.saveVideoDetailsForm = new FormGroup({
       title: this.title,
@@ -97,5 +100,21 @@ export class SaveVideoDetailsComponent implements OnInit{
         //show an upload success notification.
         this.matSnackBar.open("Thumbnail Upload Successful", "OK")
       })
+  }
+
+  saveVideo() {
+    //make a call to video service to make a http call to our backend
+    const videoMetaData: VideoDto = {
+      "id": this.videoId,
+      "title": this.saveVideoDetailsForm.get('title')?.value,
+      "description": this.saveVideoDetailsForm.get('description')?.value,
+      "tags": this.tags,
+      "videoStatus": this.saveVideoDetailsForm.get('videoStatus')?.value,
+      "videoUrl": this.videoUrl,
+      "thumbnailUrl": this.thumbnailUrl
+    }
+    this.videoService.saveVideo(videoMetaData).subscribe(data =>{
+      this.matSnackBar.open("video Metadata Updated successfully", "OK")
+    })
   }
 }
--- fronted/youtube-ui/src/app/video-dto.ts
+++ fronted/youtube-ui/src/app/video-dto.ts
@@ -1,5 +1,6 @@
 export interface VideoDto{
   id: string;
+  title: String;
   description: string;
   tags: Array<string>;
   videoUrl: string;
